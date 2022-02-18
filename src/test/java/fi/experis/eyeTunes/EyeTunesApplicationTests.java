9060d862dfaba7d77d720c9daae3a6c17f3118f9
Sasu Korhonen <smage90@hotmail.com>
2022-02-18 08:54:30 +0200

    Inital comit

---
+++ src/test/java/fi/experis/eyeTunes/EyeTunesApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.experis.eyeTunes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EyeTunesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
