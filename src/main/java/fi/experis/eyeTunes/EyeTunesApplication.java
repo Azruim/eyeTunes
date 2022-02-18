316fa65ea0eae67ec8c2d272497d67ef7bd2efcc
Sasu Korhonen <smage90@hotmail.com>
2022-02-18 09:01:46 +0200

    Revert "Revert "Inital comit""
    This reverts commit b9d2b944b708e9991977447218df23150d74fef1.

---
+++ src/main/java/fi/experis/eyeTunes/EyeTunesApplication.java
@@ -0,0 +1,13 @@
+package fi.experis.eyeTunes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EyeTunesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EyeTunesApplication.class, args);
+	}
+
+}
