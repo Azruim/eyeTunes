a75cca119b34aa0faaa99c4312ae5e7512ece47f
Sasu Korhonen <smage90@hotmail.com>
2022-02-18 10:53:44 +0200

    feat: Application now starts

--- src/main/java/fi/experis/eyeTunes/EyeTunesApplication.java
+++ src/main/java/fi/experis/eyeTunes/EyeTunesApplication.java
@@ -2,8 +2,9 @@ package fi.experis.eyeTunes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class EyeTunesApplication {
 
 	public static void main(String[] args) {
