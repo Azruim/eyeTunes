75aecf9554ae75836758edfee2e1b6a17e495bbe
Nico Behnen <Azruim@live.fi>
2022-02-20 19:08:24 +0200

    feat: added a method to get all customers.

--- src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
+++ src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
@@ -2,6 +2,7 @@ package fi.experis.eyeTunes.dataAccess.controllers;
 
 import fi.experis.eyeTunes.dataAccess.models.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,10 +14,13 @@ public class CustomerController {
         return "this is the index page";
     }
 
-    @GetMapping("/customers")
-    public String customers() {
-        return customerRepository.getCustomerById("1").toString();
+    @GetMapping("/customers/{customerId}")
+    public String customerById(@PathVariable String customerId) {
+        return customerRepository.getCustomerById(customerId).toString();
     }
 
-
+    @GetMapping("/customers")
+    public String allCustomers() {
+        return customerRepository.getAllCustomers().toString();
+    }
 }
--- src/main/java/fi/experis/eyeTunes/dataAccess/models/CustomerRepository.java
+++ src/main/java/fi/experis/eyeTunes/dataAccess/models/CustomerRepository.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 
 public class CustomerRepository {
     private String URL = ConnectionHelper.CONNECTION_URL;
@@ -50,5 +51,46 @@ public class CustomerRepository {
             }
         }
         return customer;
+    }
+
+    public ArrayList<Customer> getAllCustomers() {
+        ArrayList<Customer> customers = new ArrayList<>();
+        try{
+            // Connect to DB
+            conn = DriverManager.getConnection(URL);
+            System.out.println("Connection to SQLite has been established.");
+
+            // Make SQL query
+            PreparedStatement preparedStatement =
+                    conn.prepareStatement("SELECT CustomerId, FirstName, LastName , Country, PostalCode, Phone, Email FROM customer");
+
+            // Execute Query
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                customers.add(new Customer(
+                        resultSet.getLong("CustomerId"),
+                        resultSet.getString("FirstName"),
+                        resultSet.getString("LastName"),
+                        resultSet.getString("Country"),
+                        resultSet.getString("PostalCode"),
+                        resultSet.getString("Phone"),
+                        resultSet.getString("Email")
+                ));
+            }
+            System.out.println("Select specific customer successful");
+        }
+        catch (Exception exception){
+            System.out.println(exception);
+        }
+        finally {
+            try {
+                conn.close();
+            }
+            catch (Exception exception){
+                System.out.println(exception);
+            }
+        }
+        return customers;
     }
 }
