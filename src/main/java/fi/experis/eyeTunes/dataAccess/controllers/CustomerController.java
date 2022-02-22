ce6cb99c919825633b9c3ff392fbf7470319d677
Sasu Korhonen <smage90@hotmail.com>
2022-02-22 08:18:08 +0200

    feat: api/customers endpoints updated

--- src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
+++ src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
@@ -3,37 +3,34 @@ package fi.experis.eyeTunes.dataAccess.controllers;
 import fi.experis.eyeTunes.dataAccess.models.Customer;
 import fi.experis.eyeTunes.dataAccess.models.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Map;
 
 @RestController
 public class CustomerController {
     CustomerRepository customerRepository = new CustomerRepository();
 
-    @GetMapping("/")
-    public String index() {
-        return "this is the index page";
-    }
-
-    @GetMapping("/api/customer")
-    public Customer customerByParameter(@RequestParam(defaultValue = "1") String customerId,
-                                        @RequestParam(required = false) String name) {
-        if (name != null) {
+    @GetMapping("/api/customers/name/{name}")
+    public Customer customerByName(@PathVariable String name) {
             return customerRepository.getCustomerByName(name);
-        }
-        return customerRepository.getCustomerById(customerId);
-
     }
 
-    @GetMapping("/api/customers")
-    public ArrayList<Customer> allCustomers(@RequestParam(value = "limit", required = false) String limit,
-                                            @RequestParam(value = "offset",required = false) String offSet) {
-        if (limit != null || offSet != null) {
-            return customerRepository.getNumberOfCustomers(limit, offSet);
-        }
+    @GetMapping("/api/customers/id/{customerId}")
+    public Customer customerById(@PathVariable String customerId) {
+        return customerRepository.getCustomerById(customerId);
+    }
+
+    @GetMapping("/api/customers/all")
+    public ArrayList<Customer> getAllCustomers() {
         return customerRepository.getAllCustomers();
+    }
+
+    @GetMapping("/api/customers/")
+    public ArrayList<Customer> getCustomersByLimitAndOffset(@RequestParam(value = "limit", required = false) String limit,
+                                                            @RequestParam(value = "offset",required = false) String offSet) {
+        return customerRepository.getNumberOfCustomers(limit, offSet);
     }
 }
