f16651201f2da18ca6b5a81b0279f14e6f7795bc
Sasu Korhonen <smage90@hotmail.com>
2022-02-23 16:36:19 +0200

    feat: input for search || WIP

--- src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
+++ src/main/java/fi/experis/eyeTunes/dataAccess/controllers/CustomerController.java
@@ -3,6 +3,10 @@ package fi.experis.eyeTunes.dataAccess.controllers;
 import fi.experis.eyeTunes.dataAccess.models.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,16 +20,21 @@ public class CustomerController {
 
     @RequestMapping("/")
     public String home(Model model) {
+        String searchString = "";
+        model.addAttribute("searchString", searchString);
         List<Artist> artists = aRep.getRandomArtists(5);
         List<Genre> genres = gRep.getRandomGenres(5);
         List<Song> songs = sRep.getRandomSongs(5);
-        List<SearchItem> searchResults = srcRep.searchSongsByString("never");
-        for (SearchItem item :searchResults) {
-            System.out.println(item.getSong());
-        }
         model.addAttribute("artists" , artists);
         model.addAttribute("genres" , genres);
         model.addAttribute("songs" , songs);
+        return "home";
+    }
+
+    @PostMapping("/")
+    public String results(@ModelAttribute String searchString) {
+        List<SearchItem> searchItems = srcRep.searchSongsByString(searchString);
+        System.out.println(searchItems);
         return "home";
     }
 }
