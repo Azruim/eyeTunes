9bdbfc5d4d1bc1ab69b60b1aa786384b8d886acd
Sasu Korhonen <smage90@hotmail.com>
2022-02-23 14:05:37 +0200

    feat: artist, genre, track classes

---
+++ src/main/java/fi/experis/eyeTunes/dataAccess/models/Artist.java
@@ -0,0 +1,5 @@
+package fi.experis.eyeTunes.dataAccess.models;
+
+public class Artist {
+    String name;
+}
---
+++ src/main/java/fi/experis/eyeTunes/dataAccess/models/Track.java
@@ -0,0 +1,9 @@
+package fi.experis.eyeTunes.dataAccess.models;
+
+public class Track {
+    private String name;
+
+    public Track(String name) {
+        this.name = name;
+    }
+}
